2361bdc0f5103259949a7f6e7e4e30c09a71ad93
Vince <vince.hvu@gmail.com>
2026-01-03 19:06:30 -0800

    Initial commit

---
+++ src/main/java/com/vince/rtd_reliability/RtdReliabilityApplication.java
@@ -0,0 +1,13 @@
+package com.vince.rtd_reliability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtdReliabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtdReliabilityApplication.class, args);
+	}
+
+}
