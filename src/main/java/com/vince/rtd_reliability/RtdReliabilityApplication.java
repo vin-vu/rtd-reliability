32ab2a27222831bb6fe8b66ff080c927c37e7655
Vince <vince.hvu@gmail.com>
2026-01-05 19:40:37 -0800

    Add enable scheduling

--- src/main/java/com/vince/rtd_reliability/RtdReliabilityApplication.java
+++ src/main/java/com/vince/rtd_reliability/RtdReliabilityApplication.java
@@ -2,8 +2,11 @@ package com.vince.rtd_reliability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+
 public class RtdReliabilityApplication {
 
 	public static void main(String[] args) {
