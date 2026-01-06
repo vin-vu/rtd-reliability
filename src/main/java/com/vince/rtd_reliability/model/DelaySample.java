ab5ceaf5ba2b4a243dd2c2a405534ee66220b593
Vince <vince.hvu@gmail.com>
2026-01-05 19:49:20 -0800

    Update delay sample model

--- src/main/java/com/vince/rtd_reliability/model/DelaySample.java
+++ src/main/java/com/vince/rtd_reliability/model/DelaySample.java
@@ -1,7 +1,6 @@
 package com.vince.rtd_reliability.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,28 +10,21 @@ import java.time.Instant;
 @Table(name = "delay_samples")
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class DelaySample {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "route_id", nullable = false)
     private String routeId;
-
-    @Column(name = "trip_id", nullable = false)
     private String tripId;
-
-    @Column(name = "direction_id", nullable = false)
-    private Integer directionId;
-
-    @Column(name = "schedule_relationship", nullable = false)
-    private String scheduleRelationship;
-
-    @Column(name = "delay_seconds", nullable = false)
     private Integer delaySeconds;
-
-    @Column(name = "sampled_at", nullable = false)
     private Instant sampledAt;
+
+    public DelaySample(String routeId, String tripId, Integer delaySeconds, Instant sampledAt) {
+        this.routeId = routeId;
+        this.tripId = tripId;
+        this.delaySeconds = delaySeconds;
+        this.sampledAt = sampledAt;
+    }
 }
