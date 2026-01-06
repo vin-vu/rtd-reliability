7d2b979cd9de3ef9ec0d26b301932391a059fa8f
Vince <vince.hvu@gmail.com>
2026-01-05 21:36:16 -0800

    Add fetch Trip Updates method

--- src/main/java/com/vince/rtd_reliability/ingest/TripUpdatePoller.java
+++ src/main/java/com/vince/rtd_reliability/ingest/TripUpdatePoller.java
@@ -2,14 +2,18 @@ package com.vince.rtd_reliability.ingest;
 
 import com.vince.rtd_reliability.repository.DelaySampleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Slf4j
 @Component
 public class TripUpdatePoller {
 
-    private static final String TRIP_UPDATES_URL = "https://open-data.rtd-denver.com/files/gtfs-rt/rtd/TripUpdate.pb";
+    private static final String TRIP_UPDATES_URL =
+            "https://open-data.rtd-denver.com/files/gtfs-rt/rtd/TripUpdate.pb";
 
     private final RestTemplate restTemplate = new RestTemplate();
     private final DelaySampleRepository delaySampleRepository;
@@ -18,5 +22,22 @@ public class TripUpdatePoller {
         this.delaySampleRepository = delaySampleRepository;
     }
 
+    private byte[] fetchTripUpdatesBytes() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_OCTET_STREAM));
 
+        HttpEntity<Void> request = new HttpEntity<>(headers);
+
+        ResponseEntity<byte[]> response =
+                restTemplate.exchange(TRIP_UPDATES_URL, HttpMethod.GET, request, byte[].class);
+
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            log.warn(
+                    "Trip Updates HTTP code {} - message {}",
+                    response.getStatusCode(),
+                    request.getBody());
+        }
+
+        return response.getBody();
+    }
 }
