380ba582fa161cd162d157dd7b540e3315d2443c
Vince <vince.hvu@gmail.com>
2026-01-08 00:13:19 -0800

    Add scheduled poller method to get GTFS realtime bytes

--- src/main/java/com/vince/rtd_reliability/ingest/TripUpdatePoller.java
+++ src/main/java/com/vince/rtd_reliability/ingest/TripUpdatePoller.java
@@ -1,12 +1,18 @@
 package com.vince.rtd_reliability.ingest;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.transit.realtime.GtfsRealtime;
+import com.vince.rtd_reliability.model.DelaySample;
 import com.vince.rtd_reliability.repository.DelaySampleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @Component
@@ -19,9 +25,22 @@ public class TripUpdatePoller {
     private final DelaySampleRepository delaySampleRepository;
     private final AirportToUnionTripIdCache tripIdCache;
 
-    public TripUpdatePoller(DelaySampleRepository delaySampleRepository, AirportToUnionTripIdCache tripIdCache) {
+    public TripUpdatePoller(
+            DelaySampleRepository delaySampleRepository, AirportToUnionTripIdCache tripIdCache) {
         this.delaySampleRepository = delaySampleRepository;
         this.tripIdCache = tripIdCache;
+    }
+
+    @Scheduled(fixedDelay = 15_000)
+    public void pollTripUpdates() throws InvalidProtocolBufferException {
+
+        Set<String> desiredTripIds = tripIdCache.getCachedTripIds();
+        List<DelaySample> samples = new ArrayList<>();
+
+        byte[] bytes = fetchTripUpdatesBytes();
+        if (bytes == null || bytes.length == 0) return;
+
+        GtfsRealtime.FeedMessage feed = GtfsRealtime.FeedMessage.parseFrom(bytes);
     }
 
     private byte[] fetchTripUpdatesBytes() {
