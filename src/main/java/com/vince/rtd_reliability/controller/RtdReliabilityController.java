8c3c0c8b8a7849a6d391afe955ab8daaf777b85e
Vince <vince.hvu@gmail.com>
2026-01-11 15:53:13 -0800

    add get request for otp lifetime

---
+++ src/main/java/com/vince/rtd_reliability/controller/RtdReliabilityController.java
@@ -0,0 +1,34 @@
+package com.vince.rtd_reliability.controller;
+
+import com.vince.rtd_reliability.ingest.UnionTripIdCache;
+import com.vince.rtd_reliability.service.DelaySampleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Iterator;
+import java.util.Set;
+
+@RestController
+public class RtdReliabilityController {
+
+    private final DelaySampleService delaySampleService;
+    private final UnionTripIdCache unionTripIdCache;
+
+    public RtdReliabilityController(DelaySampleService delaySampleService, UnionTripIdCache
+            unionTripIdCache) {
+        this.delaySampleService = delaySampleService;
+        this.unionTripIdCache = unionTripIdCache;
+    }
+
+    @GetMapping("/otp-lifetime")
+    public void getOtpLifeTime() {
+        Set<String> cachedStopIds = unionTripIdCache.getCachedStopIds();
+        Iterator<String> iterator = cachedStopIds.iterator();
+        String stopId1 = iterator.next();
+        String stopId2 = iterator.next();
+
+        String routeId = "placeholder";
+
+        delaySampleService.getOtpLifeTime(routeId, stopId1, stopId2);
+    }
+}
