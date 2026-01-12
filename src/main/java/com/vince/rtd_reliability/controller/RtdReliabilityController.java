dbbc4dcfaac5b124c3955260ba2832ebff1c29fe
Vince <vince.hvu@gmail.com>
2026-01-11 16:23:02 -0800

    refactor otp lifetime endpoint

--- src/main/java/com/vince/rtd_reliability/controller/RtdReliabilityController.java
+++ src/main/java/com/vince/rtd_reliability/controller/RtdReliabilityController.java
@@ -4,11 +4,12 @@ import com.vince.rtd_reliability.ingest.UnionTripIdCache;
 import com.vince.rtd_reliability.model.OtpStats;
 import com.vince.rtd_reliability.service.DelaySampleService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 @RestController
 public class RtdReliabilityController {
@@ -26,12 +27,19 @@ public class RtdReliabilityController {
     }
 
     @GetMapping("/otp-lifetime")
-    public OtpStats getOtpLifeTime() {
-        Set<String> cachedStopIds = unionTripIdCache.getCachedStopIds();
-        Iterator<String> iterator = cachedStopIds.iterator();
-        String stopId1 = iterator.next();
-        String stopId2 = iterator.next();
+    public ResponseEntity<?> getOtpLifeTime() {
+        List<String> stopIds = new ArrayList<>(unionTripIdCache.getCachedStopIds());
 
-        return delaySampleService.getOtpLifeTime(routeId, stopId1, stopId2);
+        if (stopIds.size() < 2) {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body("Stop ID error (expected 2)");
+        }
+
+        Collections.sort(stopIds);
+        String stopId1 = stopIds.get(0);
+        String stopId2 = stopIds.get(1);
+
+        OtpStats stats = delaySampleService.getOtpLifeTime(routeId, stopId1, stopId2);
+        return ResponseEntity.ok(stats);
     }
 }
